e8d23cd2a3e10d9987fffd33a025d3e677c430ae
Vasic2000 <vasic2000@yandex.ru>
2019-03-04 23:02:39 +0300

    Nothing new

--- src/Lesson_2/ArraySum.java
+++ src/Lesson_2/ArraySum.java
@@ -1,10 +1,12 @@
 package Lesson_2;
 
 public class ArraySum {
-    private String[][] array = new String [4][4];
+    private String array[][] = new String[4][4];
     private int ArraySum = 0;
 
     public int summa (String[][] array) throws MyArrayDataException {
+
+
         this.array = array;
         for(int i = 0; i < array[0].length; i++)
             for(int j = 0; j < array.length; j++)
--- src/Lesson_2/Solution.java
+++ src/Lesson_2/Solution.java
@@ -2,7 +2,7 @@ package Lesson_2;
 
 public class Solution {
     public static void main(String[] args) {
-        String[][] myArray = {{"1", "2", "3","4"}, {"пять","6","7","8"}, {"1", "2", "3","4"}, {"5","6","7","8"}, {"1", "2", "3","4"}, {"5","6","7","8"}};
+        String[][] myArray = {{"1", "2", "3","4", "5"}, {"пять","6","7","8"}, {"1", "2", "3","4"}, {"5","6","7","8"}, {"1", "2", "3","4"}, {"5","6","7","8"}};
         try {
             System.out.println("Сумма элементов = " + new ArraySum().summa(myArray));
         } catch (MyArrayDataException EX) {
