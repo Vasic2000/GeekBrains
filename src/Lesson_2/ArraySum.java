6c353d2fddd308b1a51366e40d8807fda74da89c
Vasic2000 <vasic2000@yandex.ru>
2019-03-05 20:17:34 +0300

    Collect all again

--- src/Lesson_2/ArraySum.java
+++ src/Lesson_2/ArraySum.java
@@ -4,7 +4,14 @@ public class ArraySum {
     private String array[][] = new String[4][4];
     private int ArraySum = 0;
 
-    public int summa (String[][] array) throws MyArrayDataException {
+    public int summa (String[][] array) throws MyArrayDataException, MyArraySizeException {
+
+//        Столкнулся с тем, что массив [][] не обязательно ровный!
+//        В нем может быть в перввой строке 5 элементов, во второй 4...
+//        Поэтому проверяю сначала на количество строк, потом длину каждой строки (можно было бы обрезать).
+        if(array.length != 4) throw new MyArraySizeException("Массив должен быть 4 x 4\nВ нем :" + array.length + " строк!");
+        for(int i = 0; i < 4; i++) if(array[i].length != 4) throw new MyArraySizeException("Массив должен быть 4 x 4\nВ нем в " +
+                (i+1) + " строе " + array[i].length + " элементов!");
 
 
         this.array = array;
